e81ca3027dfab93922a6939e2c2599551ae97fdc
Kent ong <hkent625@gmail.com>
2024-06-23 00:54:03 -0400

    Allow vertical scrolling

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: 'src/index.html'
+      }
+    }
+  }
 });
